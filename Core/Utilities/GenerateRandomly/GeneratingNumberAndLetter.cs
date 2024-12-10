a2105335550466aa4a12b2036418b0f19aba6233
Samet Bayram Ham <sametham@outlook.com>
2024-12-10 13:16:09 +0300

    update

---
+++ Core/Utilities/GenerateRandomly/GeneratingNumberAndLetter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Core.Utilities.GenerateRandomly
+{
+    public class GeneratingNumberAndLetter
+    {
+        public static string Generate(int number)
+        {
+            Random random = new Random();
+
+            string characterPool = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+            string randomCharacter = new string(
+            Enumerable.Repeat(characterPool, number)
+                      .Select(s => s[random.Next(s.Length)])
+                      .ToArray()
+                       );
+
+            return randomCharacter;
+        }
+    }
+}
---
+++ Core/Utilities/GenerateRandomly/GeneratingNumbers.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Utilities.GenerateRandomly
+{
+    public class GeneratingNumbers
+    {
+        public static int Generate(int number)
+        {
+            Random random = new Random();
+
+            int minValue = (int)Math.Pow(10, number - 1);
+            int maxValue = (int)Math.Pow(10, number) - 1;
+
+            int randomNumber = random.Next(minValue, maxValue + 1);
+
+            return randomNumber;
+        }
+    }
+}
